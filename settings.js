ab11cd896bed3ef65000c1d6ff5ea07e4ab761b5
sakiyabot <107387116+sakiyabot@users.noreply.github.com>
2022-06-13 11:36:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['94770704840']
+global.premium = ['94770704840']
+global.ownernomer = '94770704840'
+global.ownername = '🐦ST tech show'
+global.botname = 'ST tech show'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.myweb = 'https://www.youtube.com/c/STtechshow'
+global.packname = 'ST tech show'
+global.author = '🐦Sakeesha Tharinda'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
